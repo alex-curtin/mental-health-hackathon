ed429a6821575b8d9e5aa54b42eb90a738a6637a
Alexander Curtin <alexcurtin@Alexanders-MacBook-Pro.local>
2019-09-12 17:00:57 -0400

    add models, auth, controllers

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+  before_action :authorize_request, except: %i[index show]
+  
+  def index
+    @entry = Entry.find(params[:record_id])
+    @comments = Comment.where(entry_id: @entry.id).order(created_at: :asc).includes(:user)
+    render json: @comments, :include => {:user => {:only => %i[username]}}, status: :ok
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    render json: @comment, status: :ok
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, :include => {:user => {:only => %i[username]}}, status: :created
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id]).includes(:user)
+    if @comment.update_attributes(comment_params)
+      render json: @comment, :include => {:user => {:only => %i[username]}}, status: :ok
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      head 204
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :entry_id)
+  end
+
+end
